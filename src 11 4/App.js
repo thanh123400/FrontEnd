ee5db1fe4432321b2a0b31997ad11bf052c3bb85
Rye1606Zz <49356827+thanhsonopera@users.noreply.github.com>
2023-04-11 22:18:30 +0700

    hihi

---
+++ src 11 4/App.js
@@ -0,0 +1,14 @@
+import HomePage from './HomePage';
+import App2 from './App2';
+import { Route, Routes } from 'react-router-dom';
+function App() {
+
+  return (
+    <div>
+      <HomePage/>
+      
+    </div>
+  );
+}
+
+export default App;
